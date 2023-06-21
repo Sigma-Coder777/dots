f9d01e2322e2971a61dc7ccfb68baca25176705b
Sigma-Coder777 <Sigma-Coder777@proton.me>
2023-06-21 12:30:13 +0530

    config : moved to starship prompt and remove unused font from polybar

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+starship init fish | source
 set fish_greeting
 source ~/.config/shell/aliasrc
 fish_add_path ~/.local/bin/
+fish_add_path ~/.cargo/bin/
+fish_add_path ~/.cabal/bin/
+fish_add_path ~/.ghcup/bin/
 set EDITOR "/usr/bin/nvim"
 fish_vi_key_bindings
